f672a2d0643989051143b75b99ef9a76f5c848b8
Sxnty <sxntycsgo@gmail.com>
2023-04-28 21:10:17 -0300

    feat(Added addTask function to Firestore):
    Added the addTask function to Firestore, which allows adding tasks to a tasks collection in Firestore. The function takes in a title, description, and an optional boolean for importance. The function validates the inputs using the validateTask helper function, and adds the task to Firestore using the addDoc function. The function returns a success message if the task is added successfully, or an error message if there is an error adding the task.

--- src/firebase/firestore/firestoreApi.js
+++ src/firebase/firestore/firestoreApi.js
@@ -1,4 +1,8 @@
-import { collection, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  addDoc,
+} from 'firebase/firestore';
 import { db } from '../firebase';
 
 export const getTasks = async () => {
@@ -14,5 +18,28 @@ export const getTasks = async () => {
   } catch (error) {
     console.error(error);
     return { code: 500, msg: [] };
+  }
+};
+
+const validateTask = (task) => {
+  const { title, description} = task;
+  if (!title || !description) {
+    throw new Error(
+      'Need to provide all parameters to add the task'
+    );
+  }
+};
+
+export const addTask = async ({ title, description, important }) => {
+  try {
+    important = important ?? false
+    const task = { title, description, important, done:false };
+    validateTask(task);
+    let tasksRef = collection(db, 'tasks');
+    await addDoc(tasksRef, task);
+    return { code: 200, msg: 'Task added' };
+  } catch (error) {
+    console.error(error);
+    return { code: 500, msg: `Error adding the task: ${error}` };
   }
 };
