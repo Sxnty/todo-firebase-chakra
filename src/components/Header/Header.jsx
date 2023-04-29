3e66dc64bcad875884019c3ace18b9086bb75501
Sxnty <sxntycsgo@gmail.com>
2023-04-28 21:11:31 -0300

    feat added event handler to open the modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Flex, Spacer, Heading } from '@chakra-ui/react';
 import { RiAddFill } from 'react-icons/ri';
-
+import { useState } from 'react';
+import AddTask from '../../views/AddTask';
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
+  const onOpen = () => setIsOpen(true);
+
   return (
     <Flex w={'100%'} alignItems={'center'} position={'absolute'}>
       <Heading p='1rem 3rem'>todo</Heading>
       <Spacer />
-      <RiAddFill fontSize={'3rem'} p='1rem 3rem' />
+      <RiAddFill fontSize={'3rem'} p='1rem 3rem' onClick={onOpen} cursor={'pointer'}/>
+      <AddTask isOpen={isOpen} onClose={onClose}/>
     </Flex>
   );
 }
