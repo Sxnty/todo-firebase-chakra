6d2889e02568cbe6cd71d855ef3be1252e5ea258
Sxnty <sxntycsgo@gmail.com>
2023-05-17 05:25:10 -0300

    feat added dropdown menu for edit and delete

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -11,8 +11,14 @@ import {
   Divider,
   Box,
   Checkbox,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
 } from '@chakra-ui/react';
-import { BsThreeDots } from 'react-icons/bs';
+import { HiDotsHorizontal } from 'react-icons/hi';
+import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 
 const Task = ({ title, description, important, id, done }) => {
   return (
@@ -21,7 +27,21 @@ const Task = ({ title, description, important, id, done }) => {
         <Flex align={'center'}>
           <Heading fontSize={'xl'}>{title}</Heading>
           <Spacer />
-          <BsThreeDots fontSize={'1.5rem'} />
+          <Menu>
+            <MenuButton as={Button} bg={'#FFF9DE'}>
+              <HiDotsHorizontal fontSize={'1.5rem'} />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>
+                <Text pr={'.5rem'}>Edit</Text>
+                <AiFillEdit/>
+              </MenuItem>
+              <MenuItem>
+              <Text pr={'.5rem'}>Delete</Text>
+                <AiFillDelete />
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </Flex>
       </CardHeader>
       <CardBody>
