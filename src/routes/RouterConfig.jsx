06dcd0cc05157010f2331e37edf20b94a6e211bf
Sxnty <sxntycsgo@gmail.com>
2023-05-19 17:21:39 -0300

    feat added Auth routing

--- src/routes/RouterConfig.jsx
+++ src/routes/RouterConfig.jsx
@@ -3,16 +3,30 @@ import { Routes, Route } from 'react-router-dom';
 import Login from '../views/Login';
 import TaskList from '../views/TaskList';
 import Header from '../components/Header/Header';
+import AddTask from '../views/AddTask';
+import Auth from './Auth';
 function RouterConfig() {
+  console.log('router');
   return (
     <>
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route
+          path='/add-task'
+          element={
+            <>
+              <Header />
+              <AddTask />
+            </>
+          }
+        />
+        <Route
           path='/'
           element={
             <>
-              <Header /> <TaskList />
+              <Auth>
+                <Header /> <TaskList />
+              </Auth>
             </>
           }
         />
